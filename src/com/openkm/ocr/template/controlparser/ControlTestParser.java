e6338b87067b52e9b1a64c8f131e4aa325be9cf9
Josep Llort <jllort@openkm.com>
2015-02-14 17:11:51 +0000

    initial import
    git-svn-id: http://forge.openkm.com/public/ocr-sample@7 629e130e-4a86-4a19-b771-f440e8d9c08d

---
+++ src/com/openkm/ocr/template/controlparser/ControlTestParser.java
@@ -0,0 +1,88 @@
+/**
+ * OpenKM, Open Document Management System (http://www.openkm.com)
+ * Copyright (c) 2006-2015 Paco Avila & Josep Llort
+ *
+ * No bytes were intentionally harmed during the development of this application.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.ocr.template.controlparser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.openkm.dao.bean.OCRTemplateControlField;
+import com.openkm.ocr.template.OCRParserEmptyValueException;
+import com.openkm.ocr.template.OCRTemplateControlParser;
+import com.openkm.ocr.template.OCRTemplateException;
+
+/**
+ * ControlTestParser
+ *
+ * @author jllort
+ */
+@PluginImplementation
+public class ControlTestParser implements OCRTemplateControlParser {
+	private static Logger log = LoggerFactory.getLogger(ControlTestParser.class);
+	
+	@Override
+	public boolean parse(OCRTemplateControlField otf, String text) throws OCRTemplateException, OCRParserEmptyValueException {
+		log.info("extracted text\n"+text);
+		if (text == null || text.equals("")) {
+			return false;
+		} else {
+			if (otf.getPattern() == null || otf.getPattern().equals("")) {
+				return text.toLowerCase().contains(otf.getValue()); // normalize to lowercase
+			} else {
+				Pattern pattern = Pattern.compile("(" + otf.getPattern() + ")", Pattern.UNICODE_CASE);
+				Matcher matcher = pattern.matcher(text);
+				if (matcher.find() && matcher.groupCount() == 1) {
+					if (otf.getValue().equals("")) {
+						return true;
+					} else {
+						return otf.getValue().equals(matcher.group());
+					}
+				} else {
+					return false;
+				}
+			}
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "Control test";
+	}
+
+	@Override
+	public boolean isPatternRequired() {
+		return false;
+	}
+
+	@Override
+	public String info() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Empty pattern case: true if extracted text contains field value.\n");
+		sb.append("Pattern with value field empty: true if pattern found.\n");
+		sb.append("Pattern with value field: true if pattern found equals value.\n");
+		return null;
+	}
+}
---
+++ src/com/openkm/ocr/template/parser/TestParser.java
@@ -0,0 +1,54 @@
+package com.openkm.ocr.template.parser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.openkm.dao.bean.OCRTemplateField;
+import com.openkm.ocr.template.OCRParserEmptyValueException;
+import com.openkm.ocr.template.OCRTemplateException;
+import com.openkm.ocr.template.OCRTemplateParser;
+
+/**
+ * TestParser
+ * 
+ */
+@PluginImplementation
+public class TestParser implements OCRTemplateParser {
+	private static Logger log = LoggerFactory.getLogger(TestParser.class);
+
+	@Override
+	public Object parse(OCRTemplateField otf, String text) throws OCRTemplateException, OCRParserEmptyValueException {
+		log.info("extracted text\n"+text);
+		if (text == null || text.equals("")) {
+			throw new OCRParserEmptyValueException("Empty value");
+		}
+
+		if (otf.getPattern() == null || otf.getPattern().equals("")) {
+			return text != null ? text.trim() : null;
+		} else {
+			Pattern pattern = Pattern.compile("(" + otf.getPattern() + ")", Pattern.UNICODE_CASE);
+			Matcher matcher = pattern.matcher(text);
+
+			if (matcher.find() && matcher.groupCount() == 1) {
+				return matcher.group();
+			} else {
+				throw new OCRTemplateException("Bad format, parse exception");
+			}
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "Test parser";
+	}
+
+	@Override
+	public boolean isPatternRequired() {
+		return false;
+	}
+}
